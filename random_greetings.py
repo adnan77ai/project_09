3cf0bfbcf96cce89dcae78a5eb48ada25e75754a
Adnan <yasinadnan79@gmail.com>
2022-12-11 22:24:10 +0600

    init

---
+++ random_greetings.py
@@ -0,0 +1,4 @@
+import random
+greetings = ['Hello', 'Good morning', 'Good afternoon', 'Good evening', 'Good night','Morning', 'Afternoon', 'Evening', 'Night']
+number = random.randint(0,8)
+print (greetings[number])
